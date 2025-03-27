0b6099a7ebe864f9f2cea349a1841dcb317dc476
Paul F Bugni <pbugni@u.washington.edu>
2025-03-27 12:50:01 -0700

    pluck secret directly from config and generate JWT

--- generate_JWT.py
+++ generate_JWT.py
@@ -1,12 +1,28 @@
-import jwt
 import base64
+import jwt
+import yaml
+import subprocess
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: generate_JWT.py <base64-encoded-secret-key>")
+try:
+    # capture output from docker-compose config
+    result = subprocess.run(
+        ["docker-compose", "config"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    config = yaml.safe_load(result.stdout)
+except subprocess.CalledProcessError as e:
+    print("Error calling `docker-compose config`:", e)
     sys.exit(1)
 
-secret_base64 = sys.argv[1]
+try:
+    secret_base64 = config['services']['postgrest']['environment']['PGRST_JWT_SECRET']
+except KeyError:
+    print("Error, PGRST_JWT_SECRET not defined")
+    sys.exit(1)
+
 secret = base64.b64decode(secret_base64)
 payload = {"role": "event_logger"}
 token = jwt.encode(payload, secret, algorithm="HS256")
