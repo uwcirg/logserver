0d7686327c54c72b374972d0c6448467729e4208
Paul F Bugni <pbugni@u.washington.edu>
2025-03-28 12:02:04 -0700

    removing --no-acl from pg_dump, no longer needing to recreate grants after import.  therefore no need to guard role creation clauses

--- sql/02_create-roles.sql
+++ sql/02_create-roles.sql
@@ -1,23 +1,11 @@
 
 -- create web user w/ read only auth
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'web_anon') THEN
-    create role web_anon nologin;
-  END IF;
-END $$;
-
+create role web_anon nologin;
 grant usage on schema api to web_anon;
 grant select on api.events to web_anon;
 
 -- create privileged user to write events
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'event_logger') THEN
-    create role event_logger nologin;
-  END IF;
-END $$;
-
+create role event_logger nologin;
 grant usage on schema api to event_logger;
 grant all on api.events to event_logger;
 grant usage, select on sequence api.events_id_seq to event_logger;
