ff5a72fe8e6cbbe5bd8420b083b4ec0a4a7c8664
Paul F Bugni <pbugni@u.washington.edu>
2025-03-28 10:39:00 -0700

    docker-compose is depricated; update as per review comment.

--- generate_JWT.py
+++ generate_JWT.py
@@ -5,16 +5,16 @@ import subprocess
 import sys
 
 try:
-    # capture output from docker-compose config
+    # capture output from docker compose config
     result = subprocess.run(
-        ["docker-compose", "config"],
+        ["docker compose", "config"],
         capture_output=True,
         text=True,
         check=True,
     )
     config = yaml.safe_load(result.stdout)
 except subprocess.CalledProcessError as e:
-    print("Error calling `docker-compose config`:", e)
+    print("Error calling `docker compose config`:", e)
     sys.exit(1)
 
 try:
